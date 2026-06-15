15a84a358cf6304d530fb53698e8ab7da37aca9f
hapily <87914807+hapily04@users.noreply.github.com>
2026-06-15 14:40:18 -0700

    fix kotlin problems

--- build-src/build.gradle.kts
+++ build-src/build.gradle.kts
@@ -2,6 +2,12 @@ plugins {
     `kotlin-dsl`
 }
 
+val javaVersion = System.getenv("JAVA_VERSION") ?: "25"
+
+java {
+    toolchain.languageVersion = JavaLanguageVersion.of(javaVersion)
+}
+
 repositories {
     mavenCentral()
     gradlePluginPortal()
